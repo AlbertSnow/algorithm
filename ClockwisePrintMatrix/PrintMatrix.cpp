8be681819c7a6554fc3284474282e02621ede4a6
ZhaoJiaLiang <zhaojialiang@sogou-inc.com>
2019-09-21 16:47:04 +0800

    Complete print matrix from out to inner funtion

--- ClockwisePrintMatrix/PrintMatrix.cpp
+++ ClockwisePrintMatrix/PrintMatrix.cpp
@@ -1,29 +1,73 @@
 #include <iostream>
 
-int * getInputMatrix(int n) {
-    int inputMatrix[n * n];
+int * initMatrixData(int n, int* matrix) {
+    int value = 1;
+
     for (int i = 0; i < n * n; i++)
     {
-        inputMatrix[i] = i;
+        matrix[i] = i + 1;
+    }
+    return matrix;
+}
+
+void printMatrix(int dimension, int * matrix) {
+    for (int i = 0; i < dimension * dimension; i++)
+    {
+        std::cout << matrix[i] << " ";
+        if (matrix[i] % dimension == 0)
+        {
+            std::cout << "\n";
+        }
     }
 }
 
-void printMatrix(int * n, int length) {
-    for (int i = 0; i < length; i++)
+
+void printOutline (int startNum, int dimension, int firstDimension) {
+    int currentValue = startNum;
+
+    if (dimension <= 1)
     {
-        std::cout << n[i];
-        // printf(n[i]);                                              
+        std::cout << "\n traverse is over";
+        return;
     }
     
+    std::cout << currentValue << " ";
+    for (int i = 0; i < dimension - 1; i++)
+    {
+        currentValue += 1;
+        std::cout << currentValue << "a ";
+    }
+    for (int i = 0; i < dimension - 1; i++)
+    {
+        currentValue += firstDimension;
+        std::cout << currentValue << "b ";
+    }
+    for (int i = 0; i < dimension - 1; i++)
+    {
+        currentValue -= 1;
+        std::cout << currentValue << "c ";
+    }
+    for (int i = 0; i < dimension - 2; i++)
+    {
+        currentValue -= firstDimension;
+        std::cout << currentValue << "d ";
+    }
+
+
+    printOutline(currentValue + 1, dimension - 2, firstDimension);
 }
 
 
 int main() {
-    int matrixLenght = 4;
+    int matrixDimension = 10;
+    
+    int inputMatrix[matrixDimension * matrixDimension];
+    initMatrixData(matrixDimension, inputMatrix);
+    printMatrix(matrixDimension, inputMatrix);
 
-    int* inputMatirx = getInputMatrix(matrixLenght);
 
-
-    printMatrix(inputMatirx, matrixLenght);
+    std::cout << "\n Start print matrix from out to inner \n";
+    printOutline (1, matrixDimension, matrixDimension);
+    
     return 0;
 }
