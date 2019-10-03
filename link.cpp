d0b85b55941da2dc5ede27a64ce50936b8c94b09
AlbertSnow <Albert.Snow.Email@gmail.com>
2019-10-03 20:01:37 +0800

    finish delete link node feature

--- link.cpp
+++ link.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
 struct ListNode 
@@ -57,38 +58,98 @@ void recursivePrint(ListNode& node) {
     }
 }
 
-void deleteNode(int value, ListNode *srcListNode) {
-    if (srcListNode == NULL || 
-    (srcListNode->m_next == NULL && value == srcListNode->m_nValue))
+void deleteNode(int value, ListNode& srcListNode) {
+    ListNode * listNode = &srcListNode;
+    
+    if (listNode->m_nValue == value)
     {
-        srcListNode = NULL;
+        cout << "----------------- find and delete list node -----------------";
+        srcListNode = *listNode->m_next;
         return;
     }
+    
+    
 
-       while (srcListNode->m_next != NULL) {
-        ListNode *next = srcListNode->m_next;
-        if (next->m_nValue == value)
+    while (listNode->m_next != NULL)
+    {
+        if (listNode->m_next->m_nValue == value)
         {
-            srcListNode = next->m_next;
+            cout << "----------------- find and delete list node -----------------";
+            listNode->m_next = listNode->m_next->m_next;
             return;
         }
+        
+        listNode = listNode->m_next;
+    }
+    
+
+
+}
+
+int  stringToNumber(char chars[]) {
+    int result = 0;
+    if (chars == NULL || chars[0] == '\n' || chars[0] == '-') {
+        cout << "input invalid, Support only number";
+
+        return 0;
+    } 
+
+    int digit = chars[0] - '0';                                                                                                                                                                                                                                                             
+    int i = 1;
+    while (chars[i] != '\0') {
+        digit = (chars[i] - '0') + (digit * 10);
+        cout << "digit " << digit << '\n';
+        i++;
+
+        if(i > 9) {
+            throw invalid_argument("input argument length big than 9");
+        }
+    }
+
+    return digit;
+}
+
+void testDynamicAllocate() {
+        ListNode test1;
+    cout << "\ntest: " << &test1;
+    
+    ListNode test2;
+    cout << "\ntest: " << &test2;
+
+    ListNode test3;
+    cout << "\ntest: " << &test3;
+
+    for(int i = 0; i<3; i++) {
+        ListNode test4;
+        cout << "\ntest4: " << &test4;
     }
 }
 
 int main(int argc, char* argv[]) {
     ListNode firstNode;
     int listNodeLength;
+    int deleteValue = 0;
 
     if (argc > 1)
     {
         cout << "\ninput argument: " << argv[1] << "\n";
-        listNodeLength = argv[1][0] - '0';
-        cout << "listNodeLength: " << listNodeLength << "\n";
+        listNodeLength = stringToNumber(argv[1]);
         cout << "\n";
     }
+    cout << "listNodeLength: " << listNodeLength << '\n';
+
+    if (argc > 2)
+    {
+        deleteValue = stringToNumber(argv[2]);
+    }
+    cout << "delete value: " << deleteValue << '\n';
     
     init(listNodeLength, firstNode);
     recursivePrint(firstNode);
+    deleteNode(deleteValue, firstNode);
+    recursivePrint(firstNode);
+
+    cout << '\n';
     return 0;
 }
 
