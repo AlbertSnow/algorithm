a9fc9d213b8a98e7f44adfdf5a768e125a93d242
AlbertSnow <Albert.Snow.Email@gmail.com>
2020-01-23 21:10:04 +0800

    Use inline to solve duplicate symbol error

--- Sort/SortStrategyHeader.h
+++ Sort/SortStrategyHeader.h
@@ -1,6 +1,22 @@
 
+#include <iostream>
 using namespace std;
 
+// for solve duplicate symbols
+inline void iteratorElement(int * arrays) {
+    int length = sizeof(arrays) / sizeof(arrays[0]);
+    cout << "length: " << sizeof(arrays);
+    while (length-- > 0) {
+        cout << arrays[length] << ',';
+    }
+    cout << '\n';
+}
+
+inline void swapElement(int *array, int start, int end)
+{
+    int cache = array[start];
+    array[start] = array[end];
+    array[end] = cache;
+}
+
 void sort(int *array, int start, int end);
-void swapElement(int *array, int start, int end);
-void iteratorElement(int * arrays);
